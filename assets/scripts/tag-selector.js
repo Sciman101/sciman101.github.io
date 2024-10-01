c635482864005fdd4eebbf8f875f740a5708b0d5
Sciman101 <Sciman101@proton.me>
2024-09-30 21:01:44 -0400

    Changed styling, added goodnight cohost, added tag url stuff

--- assets/scripts/tag-selector.js
+++ assets/scripts/tag-selector.js
@@ -5,10 +5,20 @@ window.onload = () => {
   allTaggedNodes = document.querySelectorAll("[data-tags]");
   noTagsWarning = document.getElementById("no-tags");
 
+  // Get tags from page hash
+  const hashTags = decodeURIComponent(window.location.hash.substring(1))
+    .split(",")
+    .filter((tag) => tag.length > 0);
+  for (const tag of hashTags) {
+    activeTagSet.add(tag);
+  }
+
   // Add initial tags
   for (const checkbox of allTagCheckboxes) {
     if (checkbox.checked) {
       activeTagSet.add(checkbox.id);
+    } else if (activeTagSet.has(checkbox.id)) {
+      checkbox.checked = true;
     }
   }
   updateVisibleNodes();
@@ -31,16 +41,16 @@ const handleTagSearch = (event) => {
   let count = 0;
   for (const checkbox of allTagCheckboxes) {
     if (text.length == 0 || checkbox.id.toLowerCase().includes(text)) {
-      checkbox.parentNode.classList.remove("hidden");
+      checkbox.parentElement.style.display = "block";
       count += 1;
     } else {
-      checkbox.parentNode.classList.add("hidden");
+      checkbox.parentElement.style.display = "none";
     }
   }
   if (count == 0) {
-    noTagsWarning.classList.remove("hidden");
+    checkbox.parentElement.style.display = "block";
   } else {
-    noTagsWarning.classList.add("hidden");
+    checkbox.parentElement.style.display = "none";
   }
 };
 
@@ -48,9 +58,11 @@ const updateVisibleNodes = () => {
   for (const node of allTaggedNodes) {
     const tagSet = new Set(node.dataset.tags.split(","));
     if (activeTagSet.size == 0 || tagSet.isSupersetOf(activeTagSet)) {
-      node.classList.remove("hidden");
+      node.style.display = "block";
     } else {
-      node.classList.add("hidden");
+      node.style.display = "none";
     }
   }
+  // Update page hash
+  window.location.hash = encodeURIComponent(Array.from(activeTagSet).join(","));
 };
